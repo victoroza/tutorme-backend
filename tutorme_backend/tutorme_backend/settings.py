89af4985d71a3ab9c91b088e9dab7cf4d3ae086e
Victor Oza <victorozadev@gmail.com>
2015-10-29 01:30:32 -0400

    using postgreSQL now

--- tutorme_backend/tutorme_backend/settings.py
+++ tutorme_backend/tutorme_backend/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'tutorme_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["TM_DB_NAME"],
+        'USER': os.environ["TM_DB_USER"],
+        'PASSWORD': os.environ["TM_DB_PASSWORD"],
+        'HOST': os.environ["TM_DB_HOST"],
+        'PORT': '5432',
     }
 }
 
